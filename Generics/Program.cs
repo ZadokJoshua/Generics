fa12df7a0ec4a61e5254eb4961053fa0235ff801
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-07 22:28:16 +0100

    StackString

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,7 @@ StackStrings();
 
 static void StackDoubles()
 {
-    var stack = new SimpleStack();
+    var stack = new SimpleStackDouble();
     stack.Push(1.2);
     stack.Push(3.8);
     stack.Push(3.0);
@@ -23,7 +23,7 @@ static void StackDoubles()
 
 static void StackStrings()
 {
-    var stack = new SimpleStack();
+    var stack = new SimpleStackString();
     stack.Push("Zadok is the boss");
     stack.Push("Merci, bien.");
     stack.Push("Joshua went to church.");
--- Generics/SimpleStack.cs
+++ Generics/SimpleStack.cs
@@ -1,13 +1,27 @@
-﻿internal class SimpleStack
+﻿public class SimpleStackDouble
 {
-    private readonly object[] _items;
+    private readonly double[] _items;
     private int _currentIndex = -1;
 
-    public SimpleStack() => _items = new object[10];
+    public SimpleStackDouble() => _items = new double[10];
 
     public int Count => _currentIndex + 1;
 
-    public void Push(object item) => _items[++_currentIndex] = item;
+    public void Push(double item) => _items[++_currentIndex] = item;
+
+    public object Pop() => _items[_currentIndex--];
+}
+
+public class SimpleStackString
+{
+    private readonly string[] _items;
+    private int _currentIndex = -1;
+
+    public SimpleStackString() => _items = new string[10];
+
+    public int Count => _currentIndex + 1;
+
+    public void Push(string item) => _items[++_currentIndex] = item;
 
     public object Pop() => _items[_currentIndex--];
 }
