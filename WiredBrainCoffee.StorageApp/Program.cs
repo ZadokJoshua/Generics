1d131ca8de668ebfa7658280a040b9bd92802169
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-08 12:47:38 +0100

    Created Generics for the employee and organization class

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+var employeeRepository = new GenericRepository<Employee>();
+employeeRepository.Add(new Employee { FirstName = "Zadok" });
+employeeRepository.Add(new Employee { FirstName = "Samuel" });
+employeeRepository.Add(new Employee { FirstName = "James" });
+
+var organizationRepository = new GenericRepository<Organization>();
+organizationRepository.Add(new Organization { Name = "GLO" });
+organizationRepository.Add(new Organization { Name = "CypherCrescent Ltd" });
+
+employeeRepository.Save();
+organizationRepository.Save(); 
+
+Console.ReadLine();
